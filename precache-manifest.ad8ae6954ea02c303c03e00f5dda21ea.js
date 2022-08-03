d4f6d7addc0725f12f8d9e9be358591e0026056e
Sugarbayar <dorjsugaraaz@gmail.com>
2022-08-03 14:47:42 +0800

    Updates

---
+++ precache-manifest.ad8ae6954ea02c303c03e00f5dda21ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d35c8d3917f9457708ebbd15997b8a59",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "f054cafefdac6f9d0a56",
+    "url": "/burger/static/css/main.162a0bc6.chunk.css"
+  },
+  {
+    "revision": "d8344b7e613fa04156e0",
+    "url": "/burger/static/js/2.a7103e70.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/burger/static/js/2.a7103e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f054cafefdac6f9d0a56",
+    "url": "/burger/static/js/main.5d65db07.chunk.js"
+  },
+  {
+    "revision": "d4390a2908c1cac1ad60",
+    "url": "/burger/static/js/runtime-main.b5950f3a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
